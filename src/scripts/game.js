c2832a9f98d0d2ae706afd60d3ac65d134f81442
Laney <laneyluong@gmail.com>
2021-12-25 15:31:38 -0800

    update css;

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,16 +1,18 @@
 export default class Game {
   constructor() {
     this.totalCost = 0;
-    this.play();
     this.display = 'none';
     this.popup = document.getElementById('popup');
     this.question = document.getElementById('question');
     this.submit = document.getElementById('submit')
+    this.play();
   }
   play() {
     const welcome = 'Welcome to Bunny Prep! This game will help you learn how to care for a bunny or rabbit. You will be assigned tasks and your goal is to keep your bunny happy and healthy. To begin, please choose which bunny you would like to adopt:'
+    this.question.innerHTML = welcome;
+    this.submit.innerHTML = 'Begin Game';
     this.togglePopup();
-    
+
   }
 
   togglePopup() {
