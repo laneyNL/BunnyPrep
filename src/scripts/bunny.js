8b6428bf7073a3ffad27a3878df25c090a218455
Laney <laneyluong@gmail.com>
2021-12-30 22:53:14 -0800

    update bunny display

--- src/scripts/bunny.js
+++ src/scripts/bunny.js
@@ -5,39 +5,19 @@ export default class Bunny {
     this.color = color;
     this.ctx = ctx;
     this.happyMeter = 5;
-    // this.loadBunnyImg();
     this.drawBunny();
     this.displayBunnyInfo();
-    this.loadBunnyImg();
-  }
-
-  loadBunnyImg() {
-    let COLORS = ['brown', 'grey', 'black'];
-    let EMOTION = ['happy', 'sad', 'mad', 'sleep'];
-    let hiddenImgs = document.querySelector('.hiddenImgs');
-    COLORS.forEach(color =>{
-      EMOTION.forEach(emotion => {
-        let img = document.createElement('img');
-        img.src = `./images/${color}/${color}_${emotion}.png`;
-        img.id = `${color}-${emotion}`;
-        img.alt = `${color} bunny`;
-        img.width = '20';
-        img.classList = 'bunny-image';
-        hiddenImgs.appendChild(img);
-      })
-    })
   }
 
   drawBunny() {
-    // console.log(this);
-    // console.log(`${this.color}-${this.emotion()}`);
-    // let bunnyImg = document.getElementById(`${this.color}-${this.emotion}`);
-
     const bunnyImg = new Image();
     bunnyImg.src = `./images/${this.color}/${this.color}_${this.emotion()}.png`;
     bunnyImg.classList ='bunny-image';
-    console.log(bunnyImg)
-    this.ctx.drawImage(bunnyImg, 200, 250, 80, 80);
+    bunnyImg.alt = `${this.emotion()} ${this.color} bunny`
+
+    bunnyImg.addEventListener('load', () => {
+      this.ctx.drawImage(bunnyImg, 200, 250, 80, 80);
+    })
   }
 
   emotion() {
