66b0fda07e4f1dd7bb3f0651259f4d3763d8a92b
Laney <laneyluong@gmail.com>
2022-01-03 12:17:01 -0800

    update lessons

--- src/scripts/lessons.js
+++ src/scripts/lessons.js
@@ -29,8 +29,8 @@ export default class Lesson {
 
   lesson0(){
     this.longDirections = `You added hay to the pile! You can move ${this.bunny.name} around with your cursor or the arrow keys. Trying moving ${this.bunny.name} to the litterbox.`;
-    this.taskBar = `Move ${this.bunny.name} to the litterbox.`;
     this.form = `<input type="submit" value='Continue'>`;
+    this.taskBar = `Move ${this.bunny.name} to the litterbox.`;
     this.info = `Bunnies can be trained to use the litterbox just like cats.`;
     this.targetFurniture = 'litter box';
   }
@@ -49,9 +49,28 @@ export default class Lesson {
       
   }
 
+
   lesson2() {
     const addHay = document.getElementById('add-hay');
     addHay.removeEventListener('click', this.lessonCompleteBinded);
+    this.longDirections = `It's mealtime! ${this.bunny.name} would like to eat some fresh veggies. Which items would you like to feed ${this.bunny.name}?`;
+    this.form = `<input type='checkbox' id='leeks' name='vegetables' required> <label for='leeks'>Leeks</label>
+    
+    <input type='checkbox' id='tomato-leaf' name='vegetables'> <label for='tomato-leaf'>Tomato Leaf</label>
+    
+    <input type='checkbox' id='cilantro' name='vegetables'> <label for='cilantro'>Cilantro</label>
+    
+    <input type='checkbox' id='iceberg' name='vegetables'> <label for='iceberg'>Iceberg Lettuce</label>
+
+    <input type='checkbox' id='carrot-leaf' name='vegetables' required> <label for='carrot-leaf'>Carrot Leaf</label>
+    
+    <input type="submit" value='Feed Vegetables'>`;
+    this.taskBar = ``;
+    this.info = ``;
+    this.targetFurniture = ``;
+  }
+  
+  lesson6() {
     this.longDirections = ``;
     // this.form = ``;
     this.taskBar = ``;
@@ -77,20 +96,16 @@ export default class Lesson {
   }
 
   lesson5() {
-    this.longDirections = ``;
-    this.form = ``;
+    this.longDirections = `Oh no. ${this.bunny.name} got dirty.How do you want to clean ${this.bunny.name}?`;
+    this.form = 
+      `<input type='radio' id='bath' name='clean-method' required> <label for='bath'>Bathe ${this.bunny.name}.</label>
+    <input type='radio' id='wipe' name='clean-method'> <label for='wipe'>Wipe ${this.bunny.name}.</label><input type="submit" value='Choose'>`;
     this.taskBar = ``;
     this.info = ``;
     this.targetFurniture = ``;
   }
 
-  lesson6() {
-    this.longDirections = ``;
-    this.form = ``;
-    this.taskBar = ``;
-    this.info = ``;
-    this.targetFurniture = ``;
-  }
+ 
 
   lesson7() {
     this.longDirections = ``;
