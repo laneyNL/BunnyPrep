edf805b2ad9bd2e57d96a735304686c00dfa813f
Laney <laneyluong@gmail.com>
2022-01-04 10:32:03 -0800

    add audio to game

--- src/index.js
+++ src/index.js
@@ -7,4 +7,17 @@ import Room from './scripts/room';
 window.addEventListener('DOMContentLoaded', (event) => {
   const canvas = document.getElementById('canvas');
   let game = new Game(canvas);
+
+  const sound = document.getElementById('volume-icon');
+  const audio = document.getElementById('audio');
+  audio.pause();
+  sound.addEventListener('click', () => {
+    if (sound.classList.value === 'fas fa-volume-mute') {
+      sound.classList = 'fas fa-volume-up';
+      audio.play();
+    } else {
+      sound.classList = 'fas fa-volume-mute';
+      audio.pause();
+    }
+  })
 });
