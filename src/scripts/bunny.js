5102cc2f2450673999b28e20601b31520055a368
Laney <laneyluong@gmail.com>
2022-01-05 09:10:24 -0800

    adjust random velocities for children

--- src/scripts/bunny.js
+++ src/scripts/bunny.js
@@ -251,6 +251,8 @@ export default class Bunny extends ConnectingObject {
     }, 500);
     const EMOTIONS = ['happy', 'sad', 'mad'];
     this.randEmotion = EMOTIONS[Math.floor(Math.random() * EMOTIONS.length)];
+    const VELS = [ -5, -3, -4, 3, 4, 5]
+    if (this.name === 'child') this.vel = [VELS[Math.floor(Math.random() * VELS.length)], VELS[Math.floor(Math.random() * VELS.length)]];
   }
 
   drawFriend() {
